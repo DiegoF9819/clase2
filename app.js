21ac3c10ea13983f830ac6f5ff90ff38827fcc87
Diego <df.giraldo24@ciaf.edu.co>
2023-08-30 20:04:25 -0500

    cambios

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 console.log("hello CIAF");
 
 console.log('hola mundo');
+
+// Definir el array de animales
+var animales = ["Elefante", "Tigre", "León", "Jirafa", "Cebra"];
+
+// Iterar a través del array usando forEach
+animales.forEach(function(animal) {
+    console.log(animal);
+});
